127687a5b94d308c9b6e8a71ecc3e1a57ef2e4c5
RAKSHIT BALIYAN <91004603+RakshitBaliyan@users.noreply.github.com>
2024-07-30 19:45:08 +0530

    files added

---
+++ script.js
@@ -0,0 +1,13 @@
+let div = document.getElementById("main");
+let para = document.getElementById("para");
+
+para.style.margin="0px";
+para.style.color="blue";
+
+div.style.backgroundColor= "yellow";
+div.style.margin = "20px";
+div.style.padding= "10px";
+div.style.fontSize= "18px";
+div.style.fontWeight= "bold";
+div.style.height= "200px";
+div.style.width="300px";
